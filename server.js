3c7dd4d72c564d210c7b0c2bb683e1beb76cc8ae
Austin Jess <ajess33@gmail.com>
2019-04-30 10:10:18 -0500

    server is up and running

--- server.js
+++ server.js
@@ -5,18 +5,18 @@ require('dotenv').config();
 const express = require('express');
 const superagent = require('superagent');
 const cors = require('cors');
-const ejs = require('ejs');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('./public/styles'));
 
 app.use(express.static('./public'));
 
 app.get('/', (req, res) => {
-
+  res.render('pages/index');
 });
 
 app.listen(PORT, () => console.log(`App is up on ${PORT}`));
